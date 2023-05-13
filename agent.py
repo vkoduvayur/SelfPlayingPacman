a755169baa70fe2f88284dc10de33457bc5ffb5f
Varun <varun.racher@gmail.com>
2023-05-12 17:19:15 -0700

    Added a file handle to agent.py and used Manhattan Distance to choose the next neighbor

--- agent.py
+++ agent.py
@@ -12,7 +12,12 @@ class Agent():
         # Actions are up, down, left, right, which are in constants.py
         # for node to be a goal, we need to know if it contains a pellet
 
+        self.f = open("path.txt", "a")
+        self.f.write("init agent\n")
+
+
     def search(self, init_state):
+        # self.f.open("path.txt", "a")
         # only conduct a search if the path is empty
         print(f"initial state: {init_state}")
         if len(self.transition_model) == 0:
@@ -31,17 +36,19 @@ class Agent():
                 for direction, next in current.neighbors.items():
                     print(f"next: {next}")
                     print(f"direction: {direction}")
-                    print(self.visited)
+                    #print(self.visited)
                     # self.goal = direction
+                    neighbors = queue.PriorityQueue()
                     # put opposite direction in the next node before putting it into frontier
                     if next not in reached and next not in self.visited and next != None:
                         print("inside if")
                         m_dist = self.ManhattanDistance(current, next)
                         print(f"Manhattan Distance: {m_dist}")
+                        neighbors.put((-m_dist, (direction, next)))
                         next.direction = -direction
                         # if direction isn't opposite of previous direction, then add new direction to transition model and goal
                         # if direction != 0 - self.goal:
-                        self.transition_model.append(direction)  # shouldn't be opposite of previous direction
+                        # self.transition_model.append(direction)  # shouldn't be opposite of previous direction
                         self.goal = direction  # shouldn't be opposite of previous direction
                         frontier.put(next)
                         reached.add(next)
@@ -49,6 +56,16 @@ class Agent():
                     else:
                         self.goal = direction
 
+                while neighbors.qsize() > 0:
+                    direction_tuple = neighbors.get()
+                    dir = direction_tuple[1][0]
+                    nxt = direction_tuple[1][1]
+                    if nxt not in self.visited and current.neighbors[self.goal] == None:
+                        self.transition_model.append(dir)
+                        self.goal = dir
+                    elif current.neighbors[self.goal] != None:
+                        self.transition_model.append(self.goal)
+        # self.f.close()
         return self.transition_model
 
     # return the distance travelled between two points travelling only vertically and/or horizontally
